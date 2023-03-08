2105de1e697200c9aad4ff5323c3ce9562d59ddd
skillgamerz <nicolascosimano@hotmail.com>
2023-03-07 21:28:31 -0300

    Se agrega el Proyecto1

--- js/products.js
+++ js/products.js
@@ -4,7 +4,7 @@ const productos = [
     nombre: "Harina",
     precio: 50,
     img:
-      "https://imgs.search.brave.com/0rADYsSqCFD-yqCFOUMxwOROnbXHQOzX6yzH2pzLuQU/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5B/bUJIUFJEUjNKVXRT/MVRqY0lTVnVRSGFI/YSZwaWQ9QXBp",
+      "https://http2.mlstatic.com/D_NQ_NP_2X_774730-MLA44960791254_022021-F.webp",
     cantidad: 1,
   },
   {
@@ -12,7 +12,7 @@ const productos = [
     nombre: "Galletitas",
     precio: 100,
     img:
-      "https://imgs.search.brave.com/55gNtIZbX6WBcYtNlQj2RALgF7MGXj5-inCG7S7AQno/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93d3cu/ZGlzdHJpYnVpZG9y/YXBvcC5jb20uYXIv/d3AtY29udGVudC91/cGxvYWRzLzIwMjAv/MDUvZ2FsbGV0aXRh/cy1jYWNoYWZhei5q/cGc",
+      "https://http2.mlstatic.com/D_NQ_NP_2X_901517-MLA52572993197_112022-F.webp",
     cantidad: 1,
   },
   {
@@ -20,7 +20,7 @@ const productos = [
     nombre: "Cerveza",
     precio: 150,
     img:
-      "https://imgs.search.brave.com/lc3-PPZWksqJxFiD1l6eNOo9mrEMhzxuih1JKx2BDp8/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5M/YXJZdUNORkFtX20w/Q09YT1JJRXFnSGFI/YSZwaWQ9QXBp",
+      "https://http2.mlstatic.com/D_NQ_NP_2X_793609-MLA46568959493_062021-F.webp",
     cantidad: 1,
   },
   {
@@ -28,7 +28,7 @@ const productos = [
     nombre: "Leche",
     precio: 200,
     img:
-      "https://imgs.search.brave.com/PhMcsRWeRAxZ1FGNiP_W_31dJNkZz1vmgwvtjQqs-YI/rs:fit:423:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC4x/Y0JHYm00cWViT3hB/NDVLZzBmTnJnQUFB/QSZwaWQ9QXBp",
+      "https://http2.mlstatic.com/D_NQ_NP_2X_799799-MLA49423081513_032022-F.webp",
     cantidad: 1,
   },
 ];
